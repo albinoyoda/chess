e768f3e07c8d5a1389db9c3f413bb2a209b7bc41
Oliver Sundell <oliver.sundell@hotmail.com>
2020-12-22 16:18:31 +0100

    small fix on performance test

--- main_test_performance.cpp
+++ main_test_performance.cpp
@@ -1,18 +1,14 @@
 #include "Moves.hpp"
 #include "Tree_search.hpp"
-#include "util/include/Chess_board.hpp"
 
 #include <ctime>
 #include <iostream>
-#include <limits>
-#include <string>
 
 int main()
 {
     // compute values when piece is taken
     // create a check-if-mate function
-    std::vector<double> times{};
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 20; i++)
     {
         clock_t clock1 = clock();
         Board_state board_state{"bT0000bQbK0000bT"
@@ -30,12 +26,9 @@ int main()
         Tree_search tree_search{config, board_state, Piece_color::black};
         tree_search.find_best_action();
         Action best_action = tree_search.get_best_action();
-
-        times.push_back((double)(clock() - clock1) / CLOCKS_PER_SEC);
-    }
-    for (auto time : times)
-    {
-        std::cout << "Measured times " << time << std::endl;
+        double time_taken = (double)(clock() - clock1) / CLOCKS_PER_SEC;
+        std::cout << "Measured time: " << time_taken << ", nodes/second = " << tree_search.queries / time_taken
+                  << std::endl;
     }
     return 0;
 }
